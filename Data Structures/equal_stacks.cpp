dc7592876603fc07acf63e19e06272a512f19327
shubham <tomarshubham@gmail.com>
2020-03-11 14:09:51 +0530

    Replacing includes with bits/stdc++

--- Data Structures/equal_stacks.cpp
+++ Data Structures/equal_stacks.cpp
@@ -1,24 +1,4 @@
-#include <map>
-#include <set>
-#include <list>
-#include <cmath>
-#include <ctime>
-#include <deque>
-#include <queue>
-#include <stack>
-#include <string>
-#include <bitset>
-#include <cstdio>
-#include <limits>
-#include <vector>
-#include <climits>
-#include <cstring>
-#include <cstdlib>
-#include <fstream>
-#include <numeric>
-#include <sstream>
-#include <iostream>
-#include <algorithm>
+#include <bits/stdc++.h>
 //#include <unordered_map>
 using namespace std;
 void input_file(char* filename, vector<int> &destination)// vector<int> &destination)
