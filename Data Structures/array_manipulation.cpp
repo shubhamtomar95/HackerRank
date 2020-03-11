a257423f8cba36a83a8741cfd972e71e6ccb4a4b
tomarshubham@gmail.com <tomarshubham@gmail.com>
2020-03-11 13:32:50 +0530

    Removed Comments

--- Data Structures/array_manipulation.cpp
+++ Data Structures/array_manipulation.cpp
@@ -4,18 +4,11 @@ using namespace std;
 int main(int argc, char const *argv[])
 {
 	std::vector<int> array(5, 0);
-	// cin >> iterations;
 	for (int i = 0; i < 3 ; ++i)
-	//for (int i = 0; i < queries.size() ; ++i)
 	{
-		// vector<int> query = queries[i];
 		int start, end, num;
 		cin >> start >> end >> num;
-		// start = query[0];
-		// end = query[1];
-		// num = query[2];
-		
-		
+
 		array[start - 1] = array[start - 1] + num;
 		array[end] = array[end] - num;
 		
